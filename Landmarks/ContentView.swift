78987883280b8c93fa81af2b99e7859c706f3cec
李懿哲 <liyizhezhe@gmail.com>
2019-07-18 13:17:37 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 李懿哲 on 2019/7/18.
+//  Copyright © 2019 犇犇科技. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
